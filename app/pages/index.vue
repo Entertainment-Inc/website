6992f39a4ce94b0115c01631a82f68cf40dfdc47
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-12-05 03:29:53 +0100

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+
+const githubUrl = 'https://github.com/Entertainment-Inc'
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-white dark:bg-gray-900 flex flex-col items-center pt-20">
+    <UCard class="p-8 text-center w-full max-w-md shadow-lg">
+      <img
+        src="/images/logo.png"
+        alt="Logo"
+        class="mx-auto mb-4 w-20 h-20 rounded-full shadow-md"
+      />  
+      <h1 class="text-3xl font-bold text-gray-800 dark:text-white">
+        Enterteinment Inc
+      </h1>
+
+      <div class="mt-4">
+        <NuxtLink
+          :href="githubUrl"
+        >
+          <UIcon 
+            :to="githubUrl"
+            name="i-lucide:github"
+            class="size-7"
+          />
+        </NuxtLink>
+      </div>
+    </UCard>
+  </div>
+</template>
